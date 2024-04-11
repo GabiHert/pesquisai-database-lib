1e1ea20a3078936070fc2868bde304c2bce4f4c2
GabiHert <hertergabriel4@gmail.com>
2024-04-11 15:49:41 -0300

    fix: connection public

--- repositories/request.go
+++ repositories/request.go
@@ -6,26 +6,26 @@ import (
 )
 
 type Request struct {
-	connection *connection.Connection
+	Connection *connection.Connection
 }
 
 func (s *Request) Create(request *models.Request) error {
-	res := s.connection.Create(request)
+	res := s.Connection.Create(request)
 	return res.Error
 }
 
 func (s *Request) GetTotals(id string) (total, totalFinishedResearched int, err error) {
 	var request models.Request
-	res := s.connection.Select("total_researches", "total_finished_researches").First(&request, id)
+	res := s.Connection.Select("total_researches", "total_finished_researches").First(&request, id)
 	return request.TotalResearches, request.TotalFinishedResearched, res.Error
 }
 
 func (s *Request) Get(id string) (request models.Request, err error) {
-	res := s.connection.First(&request, id)
+	res := s.Connection.First(&request, id)
 	return request, res.Error
 }
 
 func (s *Request) GetWithResearches(id string) (request models.Request, err error) {
-	res := s.connection.Preload("Researches").First(&request, id)
+	res := s.Connection.Preload("Researches").First(&request, id)
 	return request, res.Error
 }
--- repositories/research.go
+++ repositories/research.go
@@ -6,20 +6,20 @@ import (
 )
 
 type Research struct {
-	connection *connection.Connection
+	Connection *connection.Connection
 }
 
 func (s *Research) Create(research *models.Research) error {
-	res := s.connection.Create(research)
+	res := s.Connection.Create(research)
 	return res.Error
 }
 
 func (s *Research) UpdateStatus(id, status string) error {
-	res := s.connection.Model(&models.Research{ID: id}).UpdateColumn("status", status)
+	res := s.Connection.Model(&models.Research{ID: id}).UpdateColumn("status", status)
 	return res.Error
 }
 
 func (s *Research) Get(id string) (research models.Research, err error) {
-	res := s.connection.First(&research, id)
+	res := s.Connection.First(&research, id)
 	return research, res.Error
 }
