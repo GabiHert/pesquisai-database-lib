1504a02bde5026072c3d483f0c9e1ab7338aa01f
GabiHert <hertergabriel4@gmail.com>
2024-04-11 20:15:20 -0300

    feat: add context

--- repositories/request.go
+++ repositories/request.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"context"
 	"github.com/GabiHert/pesquisai-database-lib/connection"
 	"github.com/GabiHert/pesquisai-database-lib/models"
 )
@@ -9,23 +10,23 @@ type Request struct {
 	Connection *connection.Connection
 }
 
-func (s *Request) Create(request *models.Request) error {
-	res := s.Connection.Create(request)
+func (s *Request) Create(ctx context.Context, request *models.Request) error {
+	res := s.Connection.WithContext(ctx).Create(request)
 	return res.Error
 }
 
-func (s *Request) GetTotals(id string) (total, totalFinishedResearched int, err error) {
+func (s *Request) GetTotals(ctx context.Context, id string) (total, totalFinishedResearched int, err error) {
 	var request models.Request
-	res := s.Connection.Select("total_researches", "total_finished_researches").First(&request, id)
+	res := s.Connection.WithContext(ctx).Select("total_researches", "total_finished_researches").First(&request, id)
 	return request.TotalResearches, request.TotalFinishedResearched, res.Error
 }
 
-func (s *Request) Get(id string) (request models.Request, err error) {
-	res := s.Connection.First(&request, id)
+func (s *Request) Get(ctx context.Context, id string) (request models.Request, err error) {
+	res := s.Connection.WithContext(ctx).First(&request, id)
 	return request, res.Error
 }
 
-func (s *Request) GetWithResearches(id string) (request models.Request, err error) {
-	res := s.Connection.Preload("Researches").First(&request, id)
+func (s *Request) GetWithResearches(ctx context.Context, id string) (request models.Request, err error) {
+	res := s.Connection.WithContext(ctx).Preload("Researches").First(&request, id)
 	return request, res.Error
 }
--- repositories/research.go
+++ repositories/research.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"context"
 	"github.com/GabiHert/pesquisai-database-lib/connection"
 	"github.com/GabiHert/pesquisai-database-lib/models"
 )
@@ -9,17 +10,17 @@ type Research struct {
 	Connection *connection.Connection
 }
 
-func (s *Research) Create(research *models.Research) error {
-	res := s.Connection.Create(research)
+func (s *Research) Create(ctx context.Context, research *models.Research) error {
+	res := s.Connection.WithContext(ctx).Create(research)
 	return res.Error
 }
 
-func (s *Research) UpdateStatus(id, status string) error {
-	res := s.Connection.Model(&models.Research{ID: id}).UpdateColumn("status", status)
+func (s *Research) UpdateStatus(ctx context.Context, id, status string) error {
+	res := s.Connection.WithContext(ctx).Model(&models.Research{ID: id}).UpdateColumn("status", status)
 	return res.Error
 }
 
-func (s *Research) Get(id string) (research models.Research, err error) {
-	res := s.Connection.First(&research, id)
+func (s *Research) Get(ctx context.Context, id string) (research models.Research, err error) {
+	res := s.Connection.WithContext(ctx).First(&research, id)
 	return research, res.Error
 }
