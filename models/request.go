e21f8fd1fa0fb5d3ed8ac84b04b0fa01202dcf1b
GabiHert <hertergabriel4@gmail.com>
2024-04-15 08:18:53 -0300

    add PENDING status

--- models/request.go
+++ models/request.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Request struct {
-	ID                      string    `gorm:"type:uuid;primaryKey"`
-	Context                 string    `gorm:"type:varchar(1000)"`
-	Research                string    `gorm:"type:varchar(1000)"`
-	TotalResearches         int       `gorm:"default:0"`
-	TotalFinishedResearches int       `gorm:"default:0"`
-	Status                  string    `gorm:"type:varchar(10)"`
-	Overall                 string    `gorm:"type:text"`
-	CreatedAt               time.Time `gorm:"autoCreateTime"`
-	UpdatedAt               time.Time
+	ID                      *string    `gorm:"type:uuid;primaryKey"`
+	Context                 *string    `gorm:"type:varchar(1000)"`
+	Research                *string    `gorm:"type:varchar(1000)"`
+	TotalResearches         *int       `gorm:"default:0"`
+	TotalFinishedResearches *int       `gorm:"default:0"`
+	Status                  *string    `gorm:"type:varchar(10)"`
+	Overall                 *string    `gorm:"type:text"`
+	CreatedAt               *time.Time `gorm:"autoCreateTime"`
+	UpdatedAt               *time.Time
 	Researches              []Research `gorm:"foreignKey:RequestID"`
 }
