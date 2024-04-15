8b268ce6f1cfb4b933b42ca516321a186c752925
GabiHert <hertergabriel4@gmail.com>
2024-04-15 08:34:24 -0300

    refactor: time to ptr

--- models/research.go
+++ models/research.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Research struct {
-	ID        *string   `gorm:"type:uuid;primaryKey"`
-	RequestID *string   `gorm:"type:uuid"`
-	Title     *string   `gorm:"type:varchar(50)"`
-	Link      *string   `gorm:"type:varchar(100)"`
-	Status    *string   `gorm:"type:varchar(10);default:'PROCESSING'"`
-	Summary   *string   `gorm:"type:text"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time
+	ID        *string    `gorm:"type:uuid;primaryKey"`
+	RequestID *string    `gorm:"type:uuid"`
+	Title     *string    `gorm:"type:varchar(50)"`
+	Link      *string    `gorm:"type:varchar(100)"`
+	Status    *string    `gorm:"type:varchar(10);default:'PROCESSING'"`
+	Summary   *string    `gorm:"type:text"`
+	CreatedAt *time.Time `gorm:"autoCreateTime"`
+	UpdatedAt *time.Time
 }
