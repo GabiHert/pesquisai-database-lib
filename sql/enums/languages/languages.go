5ea6abcb1cf9ec51d2c06b7da487483ad5bf9e4d
GabiHert <hertergabriel4@gmail.com>
2024-05-19 18:43:15 -0300

    feat: add languages enum

---
+++ sql/enums/languages/languages.go
@@ -0,0 +1,171 @@
+package enumlanguages
+
+const (
+	Af   = "af"
+	Sq   = "sq"
+	Sm   = "sm"
+	Ar   = "ar"
+	Az   = "az"
+	Eu   = "eu"
+	Be   = "be"
+	Bn   = "bn"
+	Bh   = "bh"
+	Bs   = "bs"
+	Bg   = "bg"
+	Ca   = "ca"
+	zhCn = "zh-CN"
+	zhTw = "zh-TW"
+	Hr   = "hr"
+	Cs   = "cs"
+	Da   = "da"
+	Nl   = "nl"
+	En   = "en"
+	Eo   = "eo"
+	Et   = "et"
+	Fo   = "fo"
+	Fi   = "fi"
+	Fr   = "fr"
+	Fy   = "fy"
+	Gl   = "gl"
+	Ka   = "ka"
+	De   = "de"
+	El   = "el"
+	Gu   = "gu"
+	Iw   = "iw"
+	Hi   = "hi"
+	Hu   = "hu"
+	Is   = "is"
+	Id   = "id"
+	Ia   = "ia"
+	Ga   = "ga"
+	It   = "it"
+	Ja   = "ja"
+	Jw   = "jw"
+	Kn   = "kn"
+	Ko   = "ko"
+	La   = "la"
+	Lv   = "lv"
+	Lt   = "lt"
+	Mk   = "mk"
+	Ms   = "ms"
+	Mt   = "mt"
+	Mr   = "mr"
+	Ne   = "ne"
+	No   = "no"
+	Nn   = "nn"
+	Oc   = "oc"
+	Fa   = "fa"
+	Pl   = "pl"
+	PtBr = "pt-BR"
+	PtPt = "pt-PT"
+	Pa   = "pa"
+	Ro   = "ro"
+	Ru   = "ru"
+	Gd   = "gd"
+	Sr   = "sr"
+	Si   = "si"
+	Sk   = "sk"
+	Sl   = "sl"
+	Es   = "es"
+	Su   = "su"
+	Sw   = "sw"
+	Sv   = "sv"
+	Tl   = "tl"
+	Ta   = "ta"
+	Te   = "te"
+	Th   = "th"
+	Ti   = "ti"
+	Tr   = "tr"
+	Uk   = "uk"
+	Ur   = "ur"
+	Uz   = "uz"
+	Vi   = "vi"
+	Cy   = "cy"
+	Xh   = "xh"
+	Zu   = "zu"
+)
+
+var Languages = []string{
+	"af",
+	"sq",
+	"sm",
+	"ar",
+	"az",
+	"eu",
+	"be",
+	"bn",
+	"bh",
+	"bs",
+	"bg",
+	"ca",
+	"zh-CN",
+	"zh-TW",
+	"hr",
+	"cs",
+	"da",
+	"nl",
+	"en",
+	"eo",
+	"et",
+	"fo",
+	"fi",
+	"fr",
+	"fy",
+	"gl",
+	"ka",
+	"de",
+	"el",
+	"gu",
+	"iw",
+	"hi",
+	"hu",
+	"is",
+	"id",
+	"ia",
+	"ga",
+	"it",
+	"ja",
+	"jw",
+	"kn",
+	"ko",
+	"la",
+	"lv",
+	"lt",
+	"mk",
+	"ms",
+	"mt",
+	"mr",
+	"ne",
+	"no",
+	"nn",
+	"oc",
+	"fa",
+	"pl",
+	"pt-BR",
+	"pt-PT",
+	"pa",
+	"ro",
+	"ru",
+	"gd",
+	"sr",
+	"si",
+	"sk",
+	"sl",
+	"es",
+	"su",
+	"sw",
+	"sv",
+	"tl",
+	"ta",
+	"te",
+	"th",
+	"ti",
+	"tr",
+	"uk",
+	"ur",
+	"uz",
+	"vi",
+	"cy",
+	"xh",
+	"zu",
+}
--- sql/enums/status/status.go
+++ sql/enums/status/status.go
@@ -1,4 +1,4 @@
-package enums
+package enumstatus
 
 const (
 	PROCESSING = "PROCESSING"
