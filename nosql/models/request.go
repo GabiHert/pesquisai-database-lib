5e33c93bd1f69500abb5fde4fbaaeee065fe42a7
GabiHert <hertergabriel4@gmail.com>
2024-05-27 20:22:48 -0300

    feat: add sentences to model

--- nosql/models/request.go
+++ nosql/models/request.go
@@ -11,6 +11,7 @@ type Request struct {
 	Status    *string    `bson:"status,omitempty"`
 	Languages *[]string  `bson:"languages,omitempty"`
 	Locations *[]string  `bson:"locations,omitempty"`
+	Sentences *[]string  `bson:"sentences,omitempty"`
 	CreatedAt *time.Time `bson:"createdAt,omitempty"`
 	UpdatedAt *time.Time `bson:"updatedAt,omitempty"`
 }
